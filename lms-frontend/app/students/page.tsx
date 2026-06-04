dd6c5fbd3dbffc87c572442ace1973cb72d049ed
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-04 12:07:55 +0530

    implement get all student endpoint in frontend

--- lms-frontend/app/students/page.tsx
+++ lms-frontend/app/students/page.tsx
@@ -1,7 +1,22 @@
 import {useState, useEffect} from "react";
 import React from 'react'
+import axios from "axios";
 
 function Student() {
+
+    const [students, setStudents] = useState([]);
+
+    // get all students
+    const getStudents=async()=>{
+        try{
+            const res=await axios.get("http://localhost:5000/api/students");
+            setStudents(res.data);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+
   return (
     <div>page</div>
   )
