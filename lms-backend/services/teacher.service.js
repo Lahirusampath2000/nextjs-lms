52030dab88a3ea2b27aac15190c0cac26edc2354
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-16 12:00:49 +0530

    get teacher update

--- lms-backend/services/teacher.service.js
+++ lms-backend/services/teacher.service.js
@@ -2,16 +2,28 @@ const pool = require("../config/database");
 
 const getTeacher = async (req, res) => {
     try {
-        const result= await pool.query(`SELECT * FROM users WHERE role='teacher'`);
+        const result = await pool.query(`
+            SELECT 
+                u.id,
+                u.name,
+                t.subject,
+                t.phone,
+                t.qualification
+            FROM users u
+            JOIN teachers t ON t.user_id = u.id
+            WHERE u.role = 'teacher'
+        `);
+
         return res.status(200).json({
             success: true,
-            teachers: result.rows,
+            teachers: result.rows
         });
+
     } catch (err) {
         console.error(err);
         return res.status(500).json({
             success: false,
-            message: "Server error",
+            message: "Server error"
         });
     }
 };
