53bb97087a5bcb1fe5cd2d5eeb21ac3237680dcd
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-23 11:12:34 +0530

    fetch all course in fronend

---
+++ lms-frontend/app/AllCourse/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React,{useEffect, useState} from 'react'
+import api from "@/lib/axios";
+
+function AllCourse() {
+
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    const fetchCourses=async()=>{
+        try{
+            const res= await api.get(`${process.env.NEXT_PUBLIC_API_URL}/api/courses`);
+            setCourses(res.data.courses);
+            setLoading(false);
+        }catch(err){
+            setError("Failed to load courses.");
+        }finally{
+            setLoading(false);
+        }
+    }
+
+
+
+
+
+
+
+
+
+  return (
+    <div>AllCourse</div>
+  )
+}
+
+export default AllCourse
